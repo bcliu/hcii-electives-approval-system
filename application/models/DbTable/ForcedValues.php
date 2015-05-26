6055ae3e34ceadd8986a0c602d2be42d29dc3025
Chen Liu <chenliu@cmu.edu>
2015-05-25 23:16:45 -0700

    Fixed bug in updating value by checking existence first, and querying key

--- application/models/DbTable/ForcedValues.php
+++ application/models/DbTable/ForcedValues.php
@@ -6,7 +6,7 @@ class Application_Model_DbTable_ForcedValues extends Zend_Db_Table_Abstract
     protected $_name = 'forced_values';
     
     public function getValue($studentId, $type, $key) {
-        $row = $this->fetchRow("student_id = '$studentId' AND type = '$type' AND key = '$key'");
+        $row = $this->fetchRow("student_id = '$studentId' AND `type` = '$type' AND `key` = '$key'");
         return $row;
     }
 
@@ -15,6 +15,8 @@ class Application_Model_DbTable_ForcedValues extends Zend_Db_Table_Abstract
     }
 
     public function updateValue($studentId, $type, $key, $value, $notes) {
+        $existing = $this->getValue($studentId, $type, $key);
+
         $data = array(
             'student_id' => $studentId,
             'type' => $type,
@@ -23,13 +25,17 @@ class Application_Model_DbTable_ForcedValues extends Zend_Db_Table_Abstract
             'notes' => $notes
         );
 
-        $where = array(
-            "`student_id` = ?" => $studentId,
-            "`type` = ?" => $type,
-            "`key` = ?" => $key
-        );
+        if (!$existing) {
+            $this->insert($data);
+        } else {
+            $where = array(
+                "`student_id` = ?" => $studentId,
+                "`type` = ?" => $type,
+                "`key` = ?" => $key
+            );
 
-        $this->update($data, $where);
+            $this->update($data, $where);
+        }
     }
 
     public function deleteByStudentId($studentId) {
